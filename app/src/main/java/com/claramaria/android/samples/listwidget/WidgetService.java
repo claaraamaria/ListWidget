538b75eb3fb43826d9cb947042f2532e3ddac4e1
claaraa <clrstancu@gmail.com>
2020-05-01 15:51:36 +0300

    Initial commit

---
+++ app/src/main/java/com/claramaria/android/samples/listwidget/WidgetService.java
@@ -0,0 +1,11 @@
+package com.claramaria.android.samples.listwidget;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+public class WidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new WidgetAdapter(this);
+    }
+}
